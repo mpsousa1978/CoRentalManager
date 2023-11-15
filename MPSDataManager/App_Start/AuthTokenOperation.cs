1ff8e3680d1e21c4d5603c85a67fc72cb27e9b6d
marcio pereira de sousa <114409309+mpsousa1978@users.noreply.github.com>
2023-11-15 12:59:26 -0600

    Add documentation swagger to API
    Adds Swashbuckle/swagger to the API for documentation and testing

---
+++ MPSDataManager/App_Start/AuthTokenOperation.cs
@@ -0,0 +1,54 @@
+﻿using Swashbuckle.Swagger;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Description;
+
+namespace MPSDataManager.App_Start
+{
+    public class AuthTokenOperation : IDocumentFilter
+    {
+        public void Apply(SwaggerDocument swaggerDoc, SchemaRegistry schemaRegistry, IApiExplorer apiExplorer)
+        {
+            swaggerDoc.paths.Add("/token" , new PathItem
+            {
+                post = new Operation
+                {
+                    tags = new List<string> { "Auth"  },
+                    consumes = new List<string>
+                    {
+                        "application/x-www-form-urlencoded"
+                    },
+                    parameters = new List<Parameter> 
+                    { 
+                        new  Parameter 
+                        {
+                            type     = "string",
+                            name = "grant_type",
+                            required = true,
+                            @in = "formData",
+                            @default = "password",
+                        },
+                        new  Parameter
+                        {
+                            type     = "string",
+                            name = "userName",
+                            required = false,
+                            @in = "formData",
+                            @default = "marcio_sousa@hotmail.com"
+                        },
+                        new  Parameter 
+                        {
+                            type     = "string",
+                            name = "password",
+                            required = false,
+                            @in = "formData",
+                            @default = "MPsousa123."
+                        }
+                    }
+                }
+            }); 
+        }
+    }
+}
